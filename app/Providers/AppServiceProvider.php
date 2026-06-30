672e913f988e7329e0199ee8e8178799c7ea383d
sony gustia <sonygustia16@gmail.com>
2026-06-30 16:03:06 +0700

    fix secure connection form filter and tailwind css

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Memaksa semua link asset, form action, dan route menggunakan HTTPS di server Railway
+        if (config('app.env') === 'production' || env('APP_URL') && str_contains(env('APP_URL'), 'https')) {
+            URL::forceScheme('https');
+        }
     }
 }
