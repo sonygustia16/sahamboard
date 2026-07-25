f30f7e77c8ca654f6e31ca9841632ffcdcb47177
sony gustia <sonygustia16@gmail.com>
2026-07-25 07:39:00 +0700

    fix: buy avg dan sell avg broker masing masing

--- app/Http/Controllers/BrokerSummaryController.php
+++ app/Http/Controllers/BrokerSummaryController.php
@@ -31,6 +31,11 @@ class BrokerSummaryController extends Controller
      * supaya index tiap titik dijamin align 1:1 dengan chart yang sudah tampil.
      * Tidak menghitung rentang tanggal sendiri — murni ikut tanggal yang dikirim.
      *
+     * Selain net value/volume kumulatif, endpoint ini juga menghitung
+     * Buy Avg & Sell Avg KUMULATIF per broker (bval/bvol dan sval/svol
+     * dijumlahkan dari hari pertama s/d hari itu, baru dibagi), supaya
+     * konsisten dengan garis net kumulatif yang sudah ditampilkan di chart.
+     *
      * @param  Request  $request  query: dates=2026-07-01,2026-07-02,...  mode=value|volume  broker_limit=6
      */
     public function flow(Request $request, string $stockCode, BrokerSummaryService $service)
@@ -50,6 +55,10 @@ class BrokerSummaryController extends Controller
         $dates = array_slice($dates, -90);
 
         $dailyBrokerNval = [];   // [date][broker_code] => nval/nvol hari itu
+        $dailyBrokerBval = [];   // [date][broker_code] => total value beli hari itu
+        $dailyBrokerBvol = [];   // [date][broker_code] => total lot beli hari itu
+        $dailyBrokerSval = [];   // [date][broker_code] => total value jual hari itu
+        $dailyBrokerSvol = [];   // [date][broker_code] => total lot jual hari itu
         $totalAbsPerBroker = []; // untuk menentukan top N broker paling aktif
         $brokerNames = [];
 
@@ -71,6 +80,11 @@ class BrokerSummaryController extends Controller
                 $val = $mode === 'volume' ? (float) $b['nvol'] : (float) $b['nval'];
 
                 $dailyBrokerNval[$date][$code] = $val;
+                $dailyBrokerBval[$date][$code] = (float) ($b['bval'] ?? 0);
+                $dailyBrokerBvol[$date][$code] = (float) ($b['bvol'] ?? 0);
+                $dailyBrokerSval[$date][$code] = (float) ($b['sval'] ?? 0);
+                $dailyBrokerSvol[$date][$code] = (float) ($b['svol'] ?? 0);
+
                 $brokerNames[$code] = $b['broker_name'];
                 $totalAbsPerBroker[$code] = ($totalAbsPerBroker[$code] ?? 0) + abs($val);
             }
@@ -82,15 +96,34 @@ class BrokerSummaryController extends Controller
         $brokerSeries = [];
         foreach ($topBrokerCodes as $code) {
             $cumulative = 0;
+            $cumBval = 0;
+            $cumBvol = 0;
+            $cumSval = 0;
+            $cumSvol = 0;
+
             $series = [];
+            $buyAvgSeries = [];
+            $sellAvgSeries = [];
+
             foreach ($dates as $date) {
                 $cumulative += $dailyBrokerNval[$date][$code] ?? 0;
+                $cumBval += $dailyBrokerBval[$date][$code] ?? 0;
+                $cumBvol += $dailyBrokerBvol[$date][$code] ?? 0;
+                $cumSval += $dailyBrokerSval[$date][$code] ?? 0;
+                $cumSvol += $dailyBrokerSvol[$date][$code] ?? 0;
+
                 $series[] = round($cumulative, 2);
+                // Buy/Sell Avg kumulatif = total value kumulatif / total lot kumulatif s/d hari itu
+                $buyAvgSeries[] = $cumBvol > 0 ? round($cumBval / $cumBvol, 2) : null;
+                $sellAvgSeries[] = $cumSvol > 0 ? round($cumSval / $cumSvol, 2) : null;
             }
+
             $brokerSeries[] = [
                 'broker_code' => $code,
                 'broker_name' => $brokerNames[$code] ?? $code,
                 'data'        => $series,
+                'buy_avg'     => $buyAvgSeries,
+                'sell_avg'    => $sellAvgSeries,
             ];
         }
 
